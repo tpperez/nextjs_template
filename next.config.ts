2d2d0cba36dbeb51d9eecbf2e8e0b5c30530ffcb
Thiago Pereira Perez <tpperez@gmail.com>
2025-07-04 14:00:17 -0300

    feat(@next/bundle-analyzer): install

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
+import bundleAnalyzer from '@next/bundle-analyzer'
 import type { NextConfig } from 'next'
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -17,4 +22,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default withBundleAnalyzer(nextConfig)
