aa060c77f5c9f45aad36ec58efe32188cd52c304
Vinicius Ribeiro <hi@ribeiro.codes>
2025-07-08 18:15:59 -0300

    fix: add nonce attr to custom scripts

--- app/views/home/home.tsx
+++ app/views/home/home.tsx
@@ -1,9 +1,11 @@
-'use client'
+import { headers } from 'next/headers'
 
 import TechRadar from './components/tech-radar'
 
-const ViewHome = () => {
-  return <TechRadar />
+const ViewHome = async () => {
+  const nonce = (await headers()).get('x-nonce') ?? undefined
+
+  return <TechRadar nonce={nonce} />
 }
 
 export default ViewHome
