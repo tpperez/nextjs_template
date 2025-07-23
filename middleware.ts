54cb4cf5e5e044ecb06d9951c63422b7016bfb96
Thiago Pereira Perez <tpperez@gmail.com>
2025-07-23 19:12:18 -0300

    docs(support): update

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export const middleware = (request: NextRequest) => {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+const environment = process.env.NODE_ENV
 
-  const cspHeader = `
+export const middleware = (request: NextRequest) => {
+  if (environment !== 'development') {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+    const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'unsafe-inline';
@@ -17,29 +19,30 @@ export const middleware = (request: NextRequest) => {
     upgrade-insecure-requests;
   `
 
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim()
+    const contentSecurityPolicyHeaderValue = cspHeader
+      .replace(/\s{2,}/g, ' ')
+      .trim()
 
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
-  requestHeaders.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue,
-  )
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set('x-nonce', nonce)
+    requestHeaders.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue,
+    )
 
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  })
+    const response = NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    })
 
-  response.headers.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue,
-  )
+    response.headers.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue,
+    )
 
-  return response
+    return response
+  }
 }
 
 export const config = {
